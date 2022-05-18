3834b1d32b70b7f1def9cc752cc1ec842ce14384
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-18 13:03:01 +0100

    Seeding done: lunch time

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form', github: 'heartcombo/simple_form'
 
+gem 'faker'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.6'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: Restaurant::CATEGORIES.sample)
+end
